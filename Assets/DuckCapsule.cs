1ec2644d32d633ca03bc96772a3f0d0f9ec69451
Guillaume Pelletier <gpelleti@isi-mtl.com>
2021-02-13 15:13:13 -0500

    feat(DUK-33): Added DuckCapsule.cs to DuckCapsule.prefab

---
+++ Assets/DuckCapsule.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckCapsule : MonoBehaviour, IShootable
+{
+    public void OnHit() {
+        Debug.Log($"{this.name}");
+    }
+}
