883ac67619221fb18c8d71a328d2d3e826df6482
Guillaume Pelletier <gpelleti@isi-mtl.com>
2021-02-16 19:10:17 -0500

    WIP(DUK-35): working on DuckController

--- Assets/Scenes/gpelletierTestScene/DuckController.cs
+++ Assets/Scenes/gpelletierTestScene/DuckController.cs
@@ -1,91 +1,125 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
+
+[Serializable] 
+public class MinMax {
+    public float min;
+    public float max;
+
+    public MinMax(float min, float max) {
+        this.min = min;
+        this.max = max;
+    }
+}
 
 public class DuckController : MonoBehaviour, IShootable {
 
+    private enum State {
+        FLYING,
+        FLEEING,
+        DEAD
+    }
+    
     public float noPoints = 1f;
-    public float flightSpeed = 2f;
-    public float escapeTime = 2f;
-    //public Vector2 flightRange = {1f, 1.5f};
+    public float HP = 1f;
+    public float flightSpeed = 1f;
+    public float escapeTime = 10f;
+    public MinMax minMaxY = new MinMax(-1f, 10f);
+    public MinMax heighRangeIncrease = new MinMax(1f, 1.5f);
     
     [HideInInspector]
-    public Vector2 spawnZone;
+    public float spawnSize;
     
+    [SerializeField]
+    private State _state;
     private Animation _animations;
     private SphereCollider _collider;
     private Rigidbody _rb;
     private Vector3 _target;
-    private bool _isAlive = true;
-    private bool _isFalling = false;
+    private bool _isDead = false;
     
     public void Start() {
         _animations = GetComponent<Animation>();
         _collider = GetComponent<SphereCollider>();
         _rb = GetComponent<Rigidbody>();
-
+        
         _rb.detectCollisions = false;
         _animations.Play("fly");
-        
         _target = GetRandomPosUp();
+        _state = State.FLYING;
     }
         
     public void OnHit() {
-        Debug.Log($"{name} killed");
-        //Add noPoints to score
-        _isAlive = false;
-        StartCoroutine(nameof(Die));
+        HP--;
+        Debug.Log($"{name} hit");
+        
+        if (HP <= 0) {
+            Debug.Log($"{name} killed");
+            //Add noPoints to score
+            _state = State.DEAD;
+            StartCoroutine(nameof(Die));
+        }
     }
     
     private void Update() {
-        if (_isAlive && escapeTime >= 0) {
-            FlyAround();
-            escapeTime -= Time.deltaTime;
-        }
-        else if (!_isFalling){
-            StartCoroutine(nameof(FlyAway));
+        if (!_isDead) {
+            
+            if (escapeTime > 0 && _state != State.FLEEING)
+                _state = State.FLEEING;
+            
+            switch (_state) {
+                case State.FLYING:
+                    FlyAround();
+                    escapeTime -= Time.deltaTime;
+                    break;
+                
+                case State.FLEEING:
+                    if (_collider.enabled) {
+                        _collider.enabled = false;
+                        _target = new Vector3(_target.x, minMaxY.max, _target.z); 
+                    }
+                    FlyToTarget();
+                    break;
+                
+                case State.DEAD:
+                    _isDead = true;
+                    break;
+            }
         }
     }
     
     private void FixedUpdate() {
-        if (transform.position.y <= -1 || transform.position.y >= 10) {
+        if (transform.position.y <= minMaxY.min || transform.position.y >= minMaxY.max) {
             Destroy(gameObject);
         }
     }
     
     private void FlyAround() {
-        float step = flightSpeed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, _target, step);
-        
-        if (Vector3.Distance(transform.position, _target) < 0.1f) {
+        FlyToTarget();
+        if (Vector3.Distance(transform.position, _target) < 0.1f)
             _target = GetRandomPosUp();
-        }
     }
 
+    private void FlyToTarget() {
+        float step = flightSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _target, step);
+    }
+    
     private Vector3 GetRandomPosUp() {
-        float direction = transform.position.x > 0 ? spawnZone.x : spawnZone.y;
-        Vector3 pos = new Vector3(Random.Range(0, direction), transform.position.y + Random.Range(1f, 1.5f), transform.position.z);
+        float x = Random.Range(0, spawnSize);
+        float dirX = transform.position.x > 0 ? -x : x;
+        float dirY = transform.position.y + Random.Range(heighRangeIncrease.min, heighRangeIncrease.max);
+        
+        Vector3 pos = new Vector3(dirX, dirY, transform.position.z);
         transform.LookAt(pos);
+        
         return pos;
     }
-    
-    private IEnumerator FlyAway() {
-        _collider.enabled = false;
-        _target = new Vector3(transform.position.x,11, transform.position.z);
-        transform.LookAt(_target);
-        
-        while (Vector3.Distance(transform.position, _target) > 0.1f) {
-            float step = flightSpeed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, _target, step);
-            yield return new WaitForSeconds(2f);
-        }
-        
-        yield return null;
-    }
-    
+
     private IEnumerator Die() {
-        _isFalling = true;
         _collider.enabled = false;
         _animations.Play("inAirDeath");
         yield return new WaitForSeconds(_animations["inAirDeath"].length);
